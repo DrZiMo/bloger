b579286205e19752136f7c64f1aa24316544d691
DrZiMo <zuhaybhamar@gmail.com>
2024-12-18 22:37:08 +0300

    added the listen port

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import dotenv from "dotenv"
 const app = express()
 dotenv.config()
 
-const PORT = process.env.PORT;
+// listening to the port
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log("Server is running in port: " + PORT))
